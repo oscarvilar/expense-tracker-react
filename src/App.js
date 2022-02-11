4b886295d43a6d861005016271f4643144d5076a
Oscar Vilar <oscar.vilarsalvador@everis.nttdata.com>
2022-02-11 10:08:50 +0100

    global context

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Balance from './components/Balance';
 import { Header } from './components/Header';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList';
+import { GlobalProvider } from './context/GlobalState';
 
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +17,7 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
